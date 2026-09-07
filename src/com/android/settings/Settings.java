081c334dc3fce5f906c1dc65d86da1af45100b61
FundaAgent <323139829+FundaAgent@users.noreply.github.com>
2026-09-07 17:50:58 +0000

    Settings: register the Forge Validation activity + FORGE_VALIDATION_SETTINGS intent
    Change-Id: I369b9dc602f187f0c741dd1331d3c79d2a77f60f
    Signed-off-by: FundaAgent <323139829+FundaAgent@users.noreply.github.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -71,6 +71,7 @@ public class Settings extends SettingsActivity {
     */
 
     public static class MemtagPageActivity extends SettingsActivity { /* empty */}
+    public static class FundamentalIntegrityActivity extends SettingsActivity { /* empty */ }
     public static class BluetoothSettingsActivity extends SettingsActivity { /* empty */ }
     public static class BluetoothDashboardActivity extends SettingsActivity { /* empty */ }
     public static class CreateShortcutActivity extends SettingsActivity { /* empty */ }
